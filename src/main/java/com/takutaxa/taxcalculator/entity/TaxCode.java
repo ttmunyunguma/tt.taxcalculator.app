375fa47dec5c6decf4cf91c30b9a2078ff3566c6
ttmunyunguma <ttmunyunguma@gmail.com>
2023-08-22 16:38:18 +0100

    TT-9 add lombok implementation

--- src/main/java/com/takutaxa/taxcalculator/entity/TaxCode.java
+++ src/main/java/com/takutaxa/taxcalculator/entity/TaxCode.java
@@ -3,8 +3,14 @@ package com.takutaxa.taxcalculator.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class TaxCode {
     @Id
     public int id;
