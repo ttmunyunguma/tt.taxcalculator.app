0fdd7911575e2cc2399283562108151866546eea
ttmunyunguma <ttmunyunguma@gmail.com>
2023-08-23 18:03:53 +0100

    TT-9 Use response entity object

--- src/main/java/com/takutaxa/taxcalculator/controller/TaxCodeController.java
+++ src/main/java/com/takutaxa/taxcalculator/controller/TaxCodeController.java
@@ -3,6 +3,7 @@ package com.takutaxa.taxcalculator.controller;
 import com.takutaxa.taxcalculator.entity.TaxCodeDTO;
 import com.takutaxa.taxcalculator.service.TaxCodeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,8 @@ public class TaxCodeController {
     private final TaxCodeService taxCodeService;
 
     @GetMapping("/tax-codes")
-    public List<TaxCodeDTO> getAllTaxCodes(){
-        return taxCodeService.getAllTaxCodes();
+    public ResponseEntity<List<TaxCodeDTO>> getAllTaxCodes(){
+        List<TaxCodeDTO> taxCodeDTOS = taxCodeService.getAllTaxCodes();
+        return ResponseEntity.ok(taxCodeDTOS);
     }
 }
